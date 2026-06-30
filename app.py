a1ceeb5e60ed1b72b0805e0fa49e1b56e23884f9
gakashi_ <akashi.khairan.phone@gmail.com>
2026-06-30 20:49:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, request
-from flask import Response
 
 app = Flask(__name__)
 
 @app.route("/echo", methods=["POST"])
 def echo():
+    # Read the binary string sent by Build Logic
     value = request.form.get("value", "00000000")
 
-    return Response(value, status=200, mimetype="text/plain")
+    # Invert every bit
+    inverted = "".join(
+        "1" if bit == "0" else "0"
+        for bit in value
+    )
+
+    return inverted, 200, {
+        "Content-Type": "text/plain"
+    }
+
+    print("Received:", repr(value))
+    print("Sending:", repr(inverted))
+    print("Length:", len(inverted))
 
 if __name__ == "__main__":
     app.run()
