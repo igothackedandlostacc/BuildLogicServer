cfe7e3636e16966c4082682283749f12263ac56c
gakashi_ <akashi.khairan.phone@gmail.com>
2026-06-30 20:11:39 +0900

    LWSAHUJDaasdasd

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Server is running!"
-
 @app.route("/echo", methods=["POST"])
 def echo():
+    # Print everything we receive
     print("Headers:", dict(request.headers))
-    print("Content-Type:", request.content_type)
     print("Raw body:", request.get_data(as_text=True))
-    print("Form:", request.form)
-    print("JSON:", request.get_json(silent=True))
 
-    return "67676767", 200, {
-        "Content-Type": "text/plain"
-    }
+    # Try reading JSON
+    data = request.get_json(silent=True)
+
+    if data and "value" in data:
+        value = data["value"]
+    else:
+        # If nothing was received, return all zeros
+        return "00000000", 200, {"Content-Type": "text/plain"}
+
+    print("Received value:", value)
+
+    # Invert every bit
+    inverted = "".join(
+        "1" if bit == "0" else "0"
+        for bit in value
+    )
+
+    print("Sending back:", inverted)
+
+    return inverted, 200, {"Content-Type": "text/plain"}
 
 if __name__ == "__main__":
     app.run()
