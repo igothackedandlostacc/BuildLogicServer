c3858dd74503374070913b28d93be5842d88e2c3
gakashi_ <akashi.khairan.phone@gmail.com>
2026-06-30 19:53:13 +0900

    im so bad forgot the quotes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route("/echo", methods=["POST"])
 def echo():
-    data = jsonify({"value": "placeholdah})
+    data = jsonify({"value":"placeholdah"})
 
     if data is None:
         return jsonify({"value": "00000000"})
