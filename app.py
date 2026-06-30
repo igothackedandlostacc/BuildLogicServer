9b5276492637e24d984e06d124b8f83413d3c51d
gakashi_ <akashi.khairan.phone@gmail.com>
2026-06-30 20:18:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,14 @@ app = Flask(__name__)
 
 @app.route("/echo", methods=["POST"])
 def echo():
-    # Print everything we receive
+    print("===== NEW REQUEST =====")
+    print("Content-Type:", request.content_type)
     print("Headers:", dict(request.headers))
-    print("Raw body:", request.get_data(as_text=True))
+    print("Raw body:", repr(request.get_data(as_text=True)))
+    print("Form:", request.form)
+    print("Args:", request.args)
 
-    # Try reading JSON
-    data = request.get_json(silent=True)
-
-    if data and "value" in data:
-        value = data["value"]
-    else:
-        # If nothing was received, return all zeros
-        return "00000000", 200, {"Content-Type": "text/plain"}
-
-    print("Received value:", value)
-
-    # Invert every bit
-    inverted = "".join(
-        "1" if bit == "0" else "0"
-        for bit in value
-    )
-
-    print("Sending back:", inverted)
-
-    return inverted, 200, {"Content-Type": "text/plain"}
+    return "10101010", 200, {"Content-Type": "text/plain"}
 
 if __name__ == "__main__":
     app.run()
