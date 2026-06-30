16f1b1d0e4e4437c5e57a3424b37a6b776c358d3
gakashi_ <akashi.khairan.phone@gmail.com>
2026-06-30 20:59:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,20 @@ app = Flask(__name__)
 
 @app.route("/echo", methods=["POST"])
 def echo():
-    # Read the binary string sent by Build Logic
-    value = request.form.get("value", "100000000")
+    # Print everything we receive
+    print("Headers:", dict(request.headers))
+    print("Raw body:", request.get_data(as_text=True))
+
+    # Try reading JSON
+    data = request.get_json(silent=True)
+
+    if data and "value" in data:
+        value = data["value"]
+    else:
+        # If nothing was received, return all zeros
+        return "00000000", 200, {"Content-Type": "text/plain"}
+
+    print("Received value:", value)
 
     # Invert every bit
     inverted = "".join(
@@ -13,13 +25,9 @@ def echo():
         for bit in value
     )
 
-    print("Received:", repr(value))
-    print("Sending:", repr(inverted))
-    print("Length:", len(inverted))
-    
-    return inverted, 200, {
-        "Content-Type": "text/plain"
-    }
+    print("Sending back:", inverted)
+
+    return inverted, 200, {"Content-Type": "text/plain"}
 
 if __name__ == "__main__":
     app.run()
